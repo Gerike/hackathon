010a2a48e8eb689ecc67102dbaa4add52ae91b4c
gergo <gerike1995@gmail.com>
2024-06-23 09:06:03 +0200

    reads api key from localstorage

--- src/lib/api-client.js
+++ src/lib/api-client.js
@@ -1,12 +1,12 @@
 const BASE_URL = 'https://api.assisteve.com';
-const SECRET = localStorage.getItem('secret');
+const SECRET = localStorage.getItem('api-key');
 
 export class ApiClient {
 
     static async getTickets() {
         const response = await fetch(`${BASE_URL}/zendesk-tickets/`, {
             method: 'GET',
-            mode: "no-cors",
+            mode: "cors",
             headers: {
                 "Content-Type": "application/json",
                 "Authorization": SECRET
